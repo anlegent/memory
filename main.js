7cfeaa709471757aec88cf13cf8c565f6587d32f
anlegent <alegent@edenschool.fr>
2024-01-15 15:26:06 +0100

    card flip function

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-let allCards = document.querySelectorAll("#card")
+const allCards = document.querySelectorAll("#card");
+
+let flippedCards = [];
+let linkedCards = [];
+
+allCards.forEach(card => {
+card.addEventListener("click", flipChoosenCard)
+});
+
+function flipCard() {
+if (flippedCards.length < 2 && !this.classList.contains('flipped') && !linkedCards.includes(this)) {
+this.classList.add('flipped');
+flippedCards.push(this);
+  
+if (flippedCards.length === 2) {
+setTimeout(checkMatch, 1000);
+}
+}
+}
