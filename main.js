251f4f7a3de1b6b75b91f2f71abab8f4ffd76c74
anlegent <alegent@edenschool.fr>
2024-01-15 17:09:35 +0100

    finished

--- main.js
+++ main.js
@@ -3,47 +3,30 @@ const allCards = document.querySelectorAll('.card');
 let flippedCards = [];
 
 function cardFlip() {
-if (this.classList.contains('matched') || this.classList.contains('flipped')) {
-return;
+    if (this.classList.contains('matched') || this.classList.contains('flipped')) {
+        return;
 }
 flippedCards.push(this);
 
 this.textContent = this.dataset.number;
 
 if (flippedCards.length === 2) {
-
-allCards.forEach(card => card.removeEventListener('click', cardFlip));
-
-
-if (flippedCards[0].dataset.number === flippedCards[1].dataset.number) {
-
-flippedCards.forEach(card => card.classList.add('matched'));
-
-
-flippedCards = [];
-
-
-if (document.querySelectorAll('.card:not(.matched)').length === 0) {
-alert('You won!');
+    if (flippedCards[0].dataset.number === flippedCards[1].dataset.number) {
+    flippedCards.forEach(card => card.classList.add('matched'));
+    flippedCards = [];
+    if (document.querySelectorAll('.card:not(.matched)').length === 0) {
+    alert('You won!');
 }
 } else {
-
 setTimeout(() => {
-
-flippedCards.forEach(card => {
-card.textContent = '';
-card.classList.remove('flipped');
-});
-
-
-flippedCards = [];
-
-allCards.forEach(card => card.addEventListener('click', cardFlip));
-}, 1000);
+    flippedCards.forEach(card => {
+     card.textContent = '';
+     card.classList.remove('flipped');
+    });
+    flippedCards = [];
+  }, 1000);
+ }
 }
-}
-
 this.classList.add('flipped');
 }
-
 allCards.forEach(card => card.addEventListener('click', cardFlip));
