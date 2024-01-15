770ee7fd8e7f6b73d9e68263894ed9771302c9f7
anlegent <alegent@edenschool.fr>
2024-01-15 16:46:28 +0100

    reworked js and html

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
-const allCards = document.querySelectorAll("#card");
+const allCards = document.querySelectorAll('.card');
 
 let flippedCards = [];
-let linkedCards = [];
 
-allCards.forEach(card => {
-card.addEventListener("click", flipChoosenCard)
+function cardFlip() {
+if (this.classList.contains('matched') || this.classList.contains('flipped')) {
+return;
+}
+flippedCards.push(this);
+
+this.textContent = this.dataset.number;
+
+if (flippedCards.length === 2) {
+
+allCards.forEach(card => card.removeEventListener('click', cardFlip));
+
+
+if (flippedCards[0].dataset.number === flippedCards[1].dataset.number) {
+
+flippedCards.forEach(card => card.classList.add('matched'));
+
+
+flippedCards = [];
+
+
+if (document.querySelectorAll('.card:not(.matched)').length === 0) {
+alert('You won!');
+}
+} else {
+
+setTimeout(() => {
+
+flippedCards.forEach(card => {
+card.textContent = '';
+card.classList.remove('flipped');
 });
 
-function flipCard() {
-if (flippedCards.length < 2 && !this.classList.contains('flipped') && !linkedCards.includes(this)) {
+
+flippedCards = [];
+
+allCards.forEach(card => card.addEventListener('click', cardFlip));
+}, 1000);
+}
+}
+
 this.classList.add('flipped');
-flippedCards.push(this);
-  
-if (flippedCards.length === 2) {
-setTimeout(checkMatch, 1000);
 }
-}
-}
+
+allCards.forEach(card => card.addEventListener('click', cardFlip));
